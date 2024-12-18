64b53fbb7892169b593e72493d1a9eaba29cade1
ktvgs <ktvgs25@gmail.com>
2024-12-18 18:05:24 +0530

    image upload done

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,14 @@ from .forms import UploadImageForm
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UploadImageForm()
+    images = UploadedImage.objects.all()
+    return render(request, 'blog/upload_image.html', {'form': form, 'images': images})
